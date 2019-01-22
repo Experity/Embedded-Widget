910e92c05b0a65f892edf444493c6aec9d1ad8b2
Dan DeBruler <ddebruler@gmail.com>
2019-01-22 12:35:31 -0500

    Pass queue ID to queue-specific helper function (#6)

--- clockwiseWaitTimes.js
+++ clockwiseWaitTimes.js
@@ -1,34 +1,34 @@
 function beginWaitTimeQuerying(waitFetchObjects, timeBetweenUpdates) {
   var TIME_BETWEEN_UPDATES = timeBetweenUpdates || 60000; // One minute in milliseconds
 
-  // This is a helper function that looks at the returned JSON from our
-  // API call and pulls the desired value from the correct key based on
-  // which timeType was passed
-  function rawWaitValue(timeType, data) {
-    if(timeType === 'hospitalWait') return data.hospital_waits.current_wait;
-    if(timeType === 'hospitalPatientsInLine') return data.hospital_waits.queue_length;
-    if(timeType === 'hospitalTotalPatients') return data.hospital_waits.queue_total;
-    if(timeType === 'queueWait') return determineQueueTimeToAdd(waitFetchObject, data).current_wait;
-    if(timeType === 'queuePatientsInLine') return determineQueueTimeToAdd(waitFetchObject, data).current_patients_in_line;
-    if(timeType === 'queueWaitRange') return determineQueueTimeToAdd(waitFetchObject, data).current_wait_range;
-    return 'Invalid timeType';
-  }
-
   // This is a helper function that pulls the correct value from the returned
   // data and delegates further formatting to a custom callback if necessary.
   function determineTimeToAdd(waitFetchObject, data) {
     var callbackFn = waitFetchObject.formatFunction;
-    var rawValue = rawWaitValue(waitFetchObject.timeType, data)
+    var rawValue = rawWaitValue(waitFetchObject.timeType, waitFetchObject.queueId, data)
     if (typeof callbackFn !== 'function') { return rawValue }
     return callbackFn(rawValue)
   }
 
+  // This is a helper function that looks at the returned JSON from our
+  // API call and pulls the desired value from the correct key based on
+  // which timeType was passed
+  function rawWaitValue(timeType, queueId, data) {
+    if(timeType === 'hospitalWait') return data.hospital_waits.current_wait;
+    if(timeType === 'hospitalPatientsInLine') return data.hospital_waits.queue_length;
+    if(timeType === 'hospitalTotalPatients') return data.hospital_waits.queue_total;
+    if(timeType === 'queueWait') return determineQueueTimeToAdd(queueId, data).current_wait;
+    if(timeType === 'queuePatientsInLine') return determineQueueTimeToAdd(queueId, data).current_patients_in_line;
+    if(timeType === 'queueWaitRange') return determineQueueTimeToAdd(queueId, data).current_wait_range;
+    return 'Invalid timeType';
+  }
+
   // This is a helper function that finds the desired queue to grab wait
   // information from
-  function determineQueueTimeToAdd(waitFetchObject, data) {
+  function determineQueueTimeToAdd(queueId, data) {
     var numberOfQueues = data.appointment_queues.length;
     for(var k = 0; k < numberOfQueues; k++) {
-      if(data.appointment_queues[k].queue_id === waitFetchObject.queueId) {
+      if(data.appointment_queues[k].queue_id === queueId) {
         return data.appointment_queues[k].queue_waits;
       }
     }
